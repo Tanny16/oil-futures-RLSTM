a123d52c07fccaf38103366958ffbccdee382136
Shuo Qi <MagicianQi@users.noreply.github.com>
2021-05-03 05:44:27 +0000

    Update train_LSTM_regress.py

--- train_LSTM_regress.py
+++ train_LSTM_regress.py
@@ -3,36 +3,31 @@
 """
 1.中国基本面:
     Hyper parameter:
-        num_epochs: 150
+        num_epochs: 100
         lr_init: 0.001
         random_seed: 273
     Result:
-        val accuracy_score: 0.5789473684210527
-        val auc: 0.5911910669975187
-    Feature importance:
-        {'oi_chg': 0.36872435809142856, 'MA': 0.43284683923333345, 'EXPMA': 0.3691968628649999, 'MACD': 0.5335974546999999, 'close': 0.37309008646216224, 'KDJ': 0.6748949445636363, 'ADTM': 0.3099554760699999, 'oi': 0.3524929716178571, 'volume': 0.3398606367620834, 'BIAS': 0.35116056692222225, 'open': 0.24076164518181817, 'low': 0.29136689967142854, 'high': 0.20363715952500003}
+        val mse: 0.0006738732385148451
+        test mse: 0.0006357924799960034
 
 2.美国基本面:
     Hyper parameter:
-        num_epochs: 150
-        lr_init: 0.01
-        random_seed: 274
-    Result:
-        val accuracy_score: 0.5108225108225108
-        val auc: 0.512374381280936
-    Feature importance:
-        {'high': 0.9418445779999999, 'close': 0.7597495101515153, 'volume': 0.7389663831666665, 'oi_chg': 0.6804000080789475, 'ADTM': 0.7844943221428571, 'EXPMA': 0.7139335654186045, 'oi': 0.94975576284375, 'KDJ': 1.196584254, 'MACD': 1.055228342529412, 'low': 0.914448548090909, 'open': 0.7851542470999999, 'BIAS': 0.959156047, 'MA': 0.0}
-
-3.中国基本面 + 美国涨跌:
-    Hyper parameter:
-        num_epochs: 150
+        num_epochs: 100
         lr_init: 0.001
         random_seed: 273
     Result:
-        val accuracy_score: 0.6140350877192983
-        val auc: 0.6265508684863523
-    Feature importance:
-        {'oi_chg': 0.36872435809142856, 'MA': 0.43284683923333345, 'EXPMA': 0.3691968628649999, 'MACD': 0.5335974546999999, 'close': 0.37309008646216224, 'KDJ': 0.6748949445636363, 'ADTM': 0.3099554760699999, 'oi': 0.3524929716178571, 'volume': 0.3398606367620834, 'BIAS': 0.35116056692222225, 'open': 0.24076164518181817, 'low': 0.29136689967142854, 'high': 0.20363715952500003}
+        val mse: 0.00018516456519314573
+        test mse: 0.00019414878695212217
+
+3.中国基本面 + 美国涨跌:
+    Hyper parameter:
+        num_epochs: 100
+        lr_init: 0.001
+        random_seed: 273
+    Result:
+        val mse: 0.0006890569731039551
+        test mse: 0.00063317343537859
+
 """
 
 import os
@@ -49,13 +44,13 @@ from preprocessing import EmotionAnalysis, OriginalData
 
 # -----------------------Training parameters-----------------------
 
-num_epochs = 150
+num_epochs = 100
 GPU_id = "cuda:2"  # Specifies the graphics card
 device = torch.device(GPU_id if torch.cuda.is_available() else "cpu")
-lr_init = 0.01  # Initial learning rate
+lr_init = 0.001  # Initial learning rate
 weights_path = "./pkl/"  # Model save path
 weights_save_interval = 5  # Model save interval
-random_seed = 274
+random_seed = 273
 
 # -----------------------Pytorch results reproduce-----------------------
 
@@ -86,9 +81,9 @@ od.train_type = "regress"
 # # 中国基本面
 # dataset = od.transform(data_source="zh")
 # # 美国基本面
-dataset = od.transform(data_source="en")
+# dataset = od.transform(data_source="en")
 # 中国基本面 + 美国涨跌
-# dataset = od.transform_zh_with_en_label()
+dataset = od.transform_zh_with_en_label()
 
 # Split the data set
 train, others = train_test_split(dataset, test_size=0.2, random_state=777)
@@ -144,17 +139,20 @@ for epoch in range(num_epochs):
 # -----------------------val-----------------------
 
 inputs_val, _, labels_val = list(zip(*val))
-labels_val = [x.tolist()[0] for x in labels_val]
 preds_val = []
 
 model.eval()
 
-for input_val in inputs_val:
+total_loss_val = 0.0
+iteration_val = 0
+for input_val, label_val in zip(inputs_val, labels_val):
+    iteration_val += 1
     optimizer.zero_grad()
     with torch.set_grad_enabled(False):
         hidden = model.init_hidden()
         cell = model.init_cell()
         hidden = hidden.to(device)
+        label_val = label_val.to(device)
         cell = cell.to(device)
         out = None
         for i in input_val:
@@ -162,24 +160,29 @@ for input_val in inputs_val:
             out, hidden, cell = model(i, hidden, cell)
         _, out_val = torch.max(out, 1)
         preds_val.append(out_val.cpu().tolist()[0])
+        loss = criterion(out, label_val)
+        total_loss_val += loss.item()
 
 print("-------------------------------")
-print("val auc: {}".format(roc_auc_score(labels_val, preds_val)))
+print("val mse: {}".format(total_loss_val / iteration_val))
 
 # -----------------------test-----------------------
 
 inputs_test, _, labels_test = list(zip(*test))
-labels_test = [x.tolist()[0] for x in labels_test]
 preds_test = []
 
 model.eval()
 
-for input_test in inputs_test:
+total_loss_test = 0.0
+iteration_test = 0
+for input_test, label_test in zip(inputs_test, labels_test):
+    iteration_test += 1
     optimizer.zero_grad()
     with torch.set_grad_enabled(False):
         hidden = model.init_hidden()
         cell = model.init_cell()
         hidden = hidden.to(device)
+        label_test = label_test.to(device)
         cell = cell.to(device)
         out = None
         for i in input_test:
@@ -187,6 +190,9 @@ for input_test in inputs_test:
             out, hidden, cell = model(i, hidden, cell)
         _, out_val = torch.max(out, 1)
         preds_val.append(out_val.cpu().tolist()[0])
+        loss = criterion(out, label_test)
+        total_loss_test += loss.item()
+
 
 print("-------------------------------")
-print("test auc: {}".format(roc_auc_score(labels_test, preds_test)))
+print("test mse: {}".format(total_loss_test / iteration_test))
